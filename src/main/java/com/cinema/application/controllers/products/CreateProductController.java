ab75a26838028fc11871a37e7ca936e571cc32df
JosineyJr <josiney.junior@semantix.ai>
2024-06-07 19:46:54 -0300

    feat✨(products): add CreateProductDTO import to CreateProductController
    Import CreateProductDTO in CreateProductController to handle product creation data transfer objects.

--- src/main/java/com/cinema/application/controllers/products/CreateProductController.java
+++ src/main/java/com/cinema/application/controllers/products/CreateProductController.java
@@ -1,6 +1,7 @@
 package com.cinema.application.controllers.products;
 
 import com.cinema.application.controllers.Controller;
+import com.cinema.application.dtos.products.CreateProductDTO;
 
 public class CreateProductController extends Controller<CreateProductDTO> {
   
