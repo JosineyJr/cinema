0eacced729123f2b1823903872d5f958d6f6a260
JosineyJr <josiney.junior@semantix.ai>
2024-06-09 12:28:55 -0300

    docs📝(PgProductInfos): add Javadoc comments to PgProductInfos class
    Add detailed Javadoc comments to the PgProductInfos class, including descriptions for constructors and getter/setter methods.

--- src/main/java/com/cinema/infra/db/postgres/entities/products/PgProductInfos.java
+++ src/main/java/com/cinema/infra/db/postgres/entities/products/PgProductInfos.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+/**
+ * Represents product information in the database.
+ */
 @Entity(name = "product_infos")
 public class PgProductInfos {
   @Id
@@ -19,36 +22,77 @@ public class PgProductInfos {
   @Column(nullable = false)
   private double price;
 
+  /**
+   * Default constructor.
+   */
   public PgProductInfos() {
   }
 
+  /**
+   * Constructor with name and price parameters.
+   * 
+   * @param name  the name of the product
+   * @param price the price of the product
+   */
   public PgProductInfos(String name, double price) {
     this.name = name;
     this.price = price;
   }
 
+  /**
+   * Constructor with ID, name, and price parameters.
+   * 
+   * @param ID    the ID of the product
+   * @param name  the name of the product
+   * @param price the price of the product
+   */
   public PgProductInfos(UUID ID, String name, double price) {
     this.ID = ID;
     this.name = name;
     this.price = price;
   }
 
+  /**
+   * Get the ID of the product.
+   * 
+   * @return the ID of the product
+   */
   public UUID getID() {
     return this.ID;
   }
 
+  /**
+   * Get the name of the product.
+   * 
+   * @return the name of the product
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Get the price of the product.
+   * 
+   * @return the price of the product
+   */
   public double getPrice() {
     return this.price;
   }
 
+  /**
+   * Set the name of the product.
+   * 
+   * @param name the name of the product
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Set the price of the product.
+   * 
+   * @param price the price of the product
+   */
   public void setPrice(double price) {
     this.price = price;
   }
