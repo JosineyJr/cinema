94f7d5bb3257d0a8129f4043bfe17b7c0fbe92e1
JosineyJr <josiney.junior@semantix.ai>
2024-06-12 23:29:50 -0300

    feat✨(genres): add edit functionality and improve UI
    Add edit functionality to genres and update UI components. Introduce ActionCellFactory for better action handling and adjust FXML for improved layout.

--- src/main/java/com/cinema/main/views/movies/EditGenreView.java
+++ src/main/java/com/cinema/main/views/movies/EditGenreView.java
@@ -4,12 +4,15 @@ import com.cinema.application.dtos.movies.GenreDTO;
 import com.cinema.application.dtos.movies.UpdateGenreDTO;
 import com.cinema.application.helpers.Response;
 import com.cinema.main.factories.movies.UpdateGenreFactory;
+import com.cinema.main.views.StageManager;
 import com.cinema.main.views.helpers.AlertError;
 import com.cinema.main.views.helpers.AlertSuccess;
+import com.cinema.main.views.helpers.ChangeWindow;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class EditGenreView {
   @FXML
@@ -25,7 +28,7 @@ public class EditGenreView {
   }
 
   @FXML
-  void editGenre(ActionEvent event) {
+  void editGenre(ActionEvent event) throws Exception{
     GenreDTO genre = new GenreDTO(this.genre.getID(), genreNameField.getText());
 
     UpdateGenreDTO updateGenreDTO = new UpdateGenreDTO(genre.getID().toString(), genreNameField.getText());
@@ -34,6 +37,10 @@ public class EditGenreView {
 
     if (response.getStatusCode() == 204) {
       new AlertSuccess("Gênero editado com sucesso!");
+
+      Stage primaryStage = StageManager.getPrimaryStage();
+
+      ChangeWindow.changeScene(primaryStage, "/com/cinema/main/views/movies/editGenre.fxml");
     } else {
       new AlertError(response.getData().toString());
     }
