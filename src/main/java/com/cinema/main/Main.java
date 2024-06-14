ac07b738c96b90a104d20a6533204de9a43e34e9
JosineyJr <josiney.junior@semantix.ai>
2024-06-14 14:37:20 -0300

    fix🐛(main): correct FXML resource path in Main.java
    Fix incorrect FXML resource path in Main.java to load the correct sales counter view instead of income reports view.

--- src/main/java/com/cinema/main/Main.java
+++ src/main/java/com/cinema/main/Main.java
@@ -44,7 +44,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         FXMLLoader fxmlLoader = new FXMLLoader(
-                getClass().getResource("/com/cinema/main/views/financial/incomeReports.fxml"));
+                getClass().getResource("/com/cinema/main/views/sales/listAvailablesSalesCounter.fxml"));
 
         Parent root = fxmlLoader.load();
         Image appIcon = new Image(getClass().getResource("/cinemaLogo.png").toString());
