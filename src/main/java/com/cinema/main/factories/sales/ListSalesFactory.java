4d63dbc5b671636211e03fed734f1aee9a3d5e59
JosineyJr <josiney.junior@semantix.ai>
2024-06-13 22:14:35 -0300

    feat✨(sales): add sales table view and initialize method
    Introduce a new sales table view in ListSalesView.java with corresponding FXML updates. Added initialize method to populate the table with sales data.

---
+++ src/main/java/com/cinema/main/factories/sales/ListSalesFactory.java
@@ -0,0 +1,23 @@
+package com.cinema.main.factories.sales;
+
+import com.cinema.application.controllers.Controller;
+import com.cinema.application.controllers.sales.ListSalesController;
+import com.cinema.application.decorators.DbTransactionController;
+import com.cinema.domain.usecases.sale.ListSalesUseCase;
+import com.cinema.infra.db.postgres.helpers.PgConnection;
+import com.cinema.infra.db.postgres.repositores.sale.PgSaleRepository;
+import com.cinema.main.factories.db.PgConnectionFactory;
+
+public class ListSalesFactory {
+  public static Controller<?> make() {
+    PgSaleRepository pgSaleRepository = new PgSaleRepository();
+
+    ListSalesUseCase listSalesUseCase = new ListSalesUseCase(pgSaleRepository);
+
+    ListSalesController listSalesController = new ListSalesController(listSalesUseCase);
+
+    PgConnection pgConnection = PgConnectionFactory.make();
+
+    return new DbTransactionController<>(listSalesController, pgConnection);
+  }
+}
